340303e5b78de8da081fd9a7e5f30fd434e638f3
Remi Jonkman <rb.jonkman@gmail.com>
2018-09-19 20:44:26 +0200

    Newest code

--- matrixMultiplication/matrixMultiplication.ino
+++ matrixMultiplication/matrixMultiplication.ino
@@ -46,21 +46,24 @@ const Matrix13 B = {
 };
 
 // Create an empty result matrix (all 0 initialized) of 13 by 13 with 16 bit unsigned integers
+const Matrix13 transposed = {
+  .content = {
+    {103,121,77,98,63,29,105,86,61,52,10,120,101},
+    {30,16,70,81,86,71,62,46,108,21,102,81,103},
+    {41,54,73,65,4,100,120,16,126,13,16,69,39},
+    {69,73,41,5,16,72,39,93,110,31,59,74,106},
+    {98,66,92,81,38,25,114,26,8,58,117,64,115},
+    {13,58,62,6,53,11,38,100,85,10,0,55,15},
+    {72,111,62,42,110,112,105,63,67,60,1,70,88},
+    {23,1,100,115,11,4,56,116,36,6,76,61,75},
+    {82,110,115,122,73,120,125,93,25,13,84,43,24},
+    {72,100,87,14,49,94,11,64,52,82,72,42,118},
+    {108,126,18,60,107,111,30,108,43,84,13,17,41},
+    {96,50,125,121,7,16,17,44,26,81,95,90,75},
+    {25,79,91,112,114,2,37,21,78,88,120,59,104},
+  },
+};
 Matrix<uint32_t, 13, 13> result = {};
-
-Matrix<int8_t, 2, 2> AExample = {
-  .content = {
-    {1, 2},
-    {4, 3}
-  }
-};
-Matrix<int8_t, 2, 3> BExample = {
-  .content = {
-    {1, 2, 3},
-    {3, -4, 7}
-  }
-};
-Matrix<int32_t, 2, 3> resultExample = {};
 
 template<typename T> 
 void printMatrixToSerial(T& matrix) {
@@ -84,6 +87,10 @@ void printMatrixToSerial(T& matrix) {
 
 template<typename T, typename U, typename V>
 inline void multiply(T& A, U& B, V& C) {
+  // Make sure result array is empty
+
+  // Don't use memset, terribly slow
+  //memset(C.content, 0, C.size * sizeof(C.content[0][0]));
   for (size_t i = 0; i < A.rows; i++) {
     for (size_t j = 0; j < B.cols; j++) {
       C.content[i][j] = 0;
@@ -97,24 +104,30 @@ inline void multiply(T& A, U& B, V& C) {
   }
 }
 
+// Multiply with transposed B
 template<typename T, typename U, typename V>
-inline void multiply2(T& A, U& B, V& C) {
+inline void multiplyWithTranpose(T& A, U& B, V& C) {
+  // Ensure result array is empty
+  //memset(C.content, 0, C.size * sizeof(C.content[0][0]));
   for (size_t i = 0; i < A.rows; i++) {
     for (size_t j = 0; j < B.cols; j++) {
-      C.content[i][j] = A.content[i][0] * B.content[0][j]
-        + A.content[i][1] * B.content[1][j]
-        + A.content[i][2] * B.content[2][j]
-        + A.content[i][3] * B.content[3][j]
-        + A.content[i][4] * B.content[4][j]
-        + A.content[i][5] * B.content[5][j]
-        + A.content[i][6] * B.content[6][j]
-        + A.content[i][7] * B.content[7][j]
-        + A.content[i][8] * B.content[8][j]
-        + A.content[i][9] * B.content[9][j]
-        + A.content[i][10] * B.content[10][j]
-        + A.content[i][11] * B.content[11][j]
-        + A.content[i][12] * B.content[12][j]
-        + (A.content[i][j] << 1);
+      // Clear element
+      C.content[i][j] = 0;
+      for(size_t r = 0; r < B.rows; r++) {
+        // Calculate matrix elements
+        // For more info, check section definition (last accessed 22-9-2018): https://en.wikipedia.org/wiki/Matrix_multiplication
+        C.content[i][j] += A.content[i][r] * B.content[j][r];
+      }
+      C.content[i][j] += (A.content[i][j] << 1);
+    }
+  }
+}
+
+template<typename T, typename U>
+inline void transpose(T& org, U& tr) {
+  for (size_t i = 0; i < A.rows; i++) {
+    for (size_t j = 0; j < B.cols; j++) {
+        tr.content[i][j] = org.content[j][i];
     }
   }
 }
@@ -137,7 +150,19 @@ void loop() {
   }
   timeVariable = micros() - timeVariable;
 
-  Serial.print("Used Time: ");
+  Serial.print("Multiply Time: ");
+  Serial.println(timeVariable);
+  printMatrixToSerial< >(result);
+
+  // Start counter
+  //transpose< >(B, transposed);
+  timeVariable = micros();
+  // Calculate the multiplication 1000 times
+  for (size_t i = 0; i < 1000; i++) {
+    multiplyWithTranpose< >(A, transposed, result);
+  }
+  timeVariable = micros() - timeVariable;
+  Serial.print("MultiplyWithTranspose Time: ");
   Serial.println(timeVariable);
   printMatrixToSerial< >(result);
   
